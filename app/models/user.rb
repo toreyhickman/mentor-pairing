e78e017e0f6eb271b06dc364c919e384e9d3a089
ezy023 <allareri@gmail.com>
2013-08-27 10:44:41 -0500

    Add twitter handle to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  attr_accessible :cohort, :email, :first_name, :last_name, :password_digest, :type
+  attr_accessible :cohort, :email, :first_name, :last_name, :password_digest, :type, :twitter_handle
 end
---
+++ db/migrate/20130827154202_add_twitter_handle_to_users_table.rb
@@ -0,0 +1,5 @@
+class AddTwitterHandleToUsersTable < ActiveRecord::Migration
+  def change
+    add_column :users, :twitter_handle, :string
+  end
+end
