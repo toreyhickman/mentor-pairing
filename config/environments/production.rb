ebbca076ac095d5db6b937c3cb4a3c0f480149a6
ezy023 <allareri@gmail.com>
2013-09-18 08:56:51 -0500

    Modify production.rb file for rail 4 assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ MentorPairing::Application.configure do
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
-  # and those relying on copy on write to perform better.
+ # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
@@ -15,14 +15,14 @@ MentorPairing::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
