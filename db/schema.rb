95cf9e343b06534a25ff1789709d3cf5e812d2cb
Torey Hickman <torey@toreyhickman.com>
2013-09-22 15:16:17 -0500

    schema.rb file from creating/migrating db

--- db/schema.rb
+++ db/schema.rb
@@ -9,39 +9,42 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130828115517) do
+ActiveRecord::Schema.define(version: 20130828115517) do
 
-  create_table "appointments", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "appointments", force: true do |t|
     t.integer  "mentor_id"
     t.integer  "mentee_id"
     t.datetime "start_time"
     t.datetime "end_time"
     t.string   "timezone"
     t.string   "location"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "availabilities", :force => true do |t|
+  create_table "availabilities", force: true do |t|
     t.datetime "start_time"
     t.datetime "end_time"
     t.integer  "mentor_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "timezone"
     t.string   "location"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "email"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "activation_code"
     t.boolean  "activated"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "twitter_handle"
   end
 
