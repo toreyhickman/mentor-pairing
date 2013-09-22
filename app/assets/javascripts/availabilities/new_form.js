f0a9f0bccf63d087eae1d007e7265dfe4e0a4616
Torey Hickman <torey@toreyhickman.com>
2013-09-22 17:41:03 -0500

    Added autopopulate to availability form on email input blur

--- app/assets/javascripts/availabilities/new_form.js
+++ app/assets/javascripts/availabilities/new_form.js
@@ -1,3 +1,24 @@
+var FindByEmail = {
+  find: function() {
+    var userEmail = {email: $('#email').val()};
+
+    $.ajax({
+      url: '/users/findmentor',
+      type: 'POST',
+      data: userEmail,
+      dataType: "JSON"
+    }).done(FindByEmail.populate);
+  },
+  populate: function(response) {
+    if (response.no_user) {}
+    else {
+      $('#first_name').val(response.first_name);
+      $('#last_name').val(response.last_name);
+      $('#twitter_handle').val(response.twitter_handle)
+    }
+  }
+}
+
 var TabHelper = {
   toggle:function(){
     $('.tab').on("click", function(e){
@@ -17,6 +38,10 @@ var TabHelper = {
   }
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
+
   TabHelper.toggle()
-})
+
+  $('#email').on('blur', FindByEmail.find)
+
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
+  require 'json'
+
   def edit
     @mentor = User.find_by_activation_code(params[:id])
     @availabilities = @mentor.availabilities.visible.order(:start_time)
     @menteeing_appointments = @mentor.menteeing_appointments.visible.order(:start_time)
     @mentoring_appointments = @mentor.mentoring_appointments.visible.order(:start_time)
+  end
+
+  def find_mentor
+      user = User.find_by_email(params[:email])
+
+      respond_to do |format|
+        format.json do
+          if user
+            render :json => { :first_name => user.first_name,
+                              :last_name => user.last_name,
+                              :twitter_handle => user.twitter_handle }
+          else
+            render :json => { :no_user => true }
+          end
+        end
+      end
   end
 end
