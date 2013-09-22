1e91507af2dfba265073141e35871cdfe154d517
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-22 15:46:04 -0500

    added ability to update user attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   def find_or_activate_by_email
     user = User.find_by_email(params[:email])
-    unless user
+    if user
+      user.update_attributes(user_params)
+    else
       user = User.create!(user_params).send_activation
       flash[:notice] = "Please go check your email, ok?"
     end
@@ -13,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def user_params
-    [:first_name, :last_name, :twitter_handle, :email].inject({}) { |hash, attribute| hash.merge(attribute => params[attribute]) }
+    [:first_name, :last_name, :twitter_handle, :email, :bio, :interests, :gravatar_url].inject({}) { |hash, attribute| hash.merge(attribute => params[attribute]) }
   end
 
 end
