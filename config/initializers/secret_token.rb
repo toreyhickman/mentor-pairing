e5304510ed093998ab0d3ebc5f6ff937d51aab7d
Ryan Briones <ryan.briones@brionesandco.com>
2013-09-26 08:53:05 -0500

    set config.secret_key_base, the Rails 4 config key for encrypting cookies

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 MentorPairing::Application.config.secret_token = '60744194ac5a5efaf1f440ef30fc3dbefceec2192925846c2f2c47412fbe0ac44e3098ebc46a5a34258a51306cbfd4eb6f9469ef9dc7cad85c5755c5627a6b67'
+MentorPairing::Application.config.secret_key_base = '187ac6e6e1c82eeb2381cd9579dfafde8e703cc2984ad2ab0b45bc24a2fdf9456c7c61f735ba0de6e13a2a3f2fc8723e7144b224c520c26299fbf863c85b2ce2'
