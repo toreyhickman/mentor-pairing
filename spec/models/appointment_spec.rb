f38b543082177b8b3c405d10b2b5b02b7a4882f2
kevinbuch <kevaustinbuch@gmail.com>
2013-10-01 17:36:28 -0500

    added kudos association and create kudos callback to appointment

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Appointment do
   it { should belong_to(:mentor) }
   it { should belong_to(:mentee) }
+  it { should have_many(:kudos) }
 
   describe "when first created" do
     before(:each) do
